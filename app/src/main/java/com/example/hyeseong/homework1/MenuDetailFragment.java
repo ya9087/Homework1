f230a54726e54655f369043d373fb50f95fd7a55
HyesungChoi <ya9087@naver.com>
2017-11-22 21:16:18 +0900

    Restaurant프레그먼트와 Menu프레그먼트 생성 by 최혜성

---
+++ app/src/main/java/com/example/hyeseong/homework1/MenuDetailFragment.java
@@ -0,0 +1,57 @@
+package com.example.hyeseong.homework1;
+
+import android.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+
+public class MenuDetailFragment extends Fragment {
+
+    static String Image;
+    static String Name;
+    static String Price;
+    static String Ex;
+
+    public void getMenu(String image, String name, String price, String ex) {
+        Image = image;
+        Name = name;
+        Price = price;
+        Ex = ex;
+    }
+
+    public MenuDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = (View) inflater.inflate(R.layout.activity_menu_detail, container, false);
+
+        ImageView img = (ImageView) rootView.findViewById(R.id.micon);
+        if (Image != null) {
+            File mPhotoFile = new File(getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), Image);
+            Uri uri = Uri.fromFile(mPhotoFile);
+            img.setImageURI(uri);
+        }
+
+        TextView name = (TextView) rootView.findViewById(R.id.mname);
+        name.setText(Name);
+
+        TextView price = (TextView) rootView.findViewById(R.id.mprice);
+        price.setText(Price + " 원");
+
+        TextView info = (TextView) rootView.findViewById(R.id.mgrade);
+        info.setText("설명: " + Ex);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/hyeseong/homework1/RestaurantDetailFragment.java
@@ -0,0 +1,27 @@
+package com.example.hyeseong.homework1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RestaurantDetailFragment extends Fragment {
+
+    public interface OnTitleSelectedListener {
+        public void onTitleSelected(int i);
+    }
+
+    public RestaurantDetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = (View) inflater.inflate(R.layout.activity_restaurant_detail, container, false);
+
+        return rootView;
+
+    }
+}
